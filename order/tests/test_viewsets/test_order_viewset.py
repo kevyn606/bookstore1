7d028d6eefc981bdf44b7971b0c28d6a1dd91331
kevyn 60 <kevynsousa606@gmail.com>
2024-02-13 13:38:29 -0300

    Adicionando autenticação por token aos testes

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -2,6 +2,7 @@ import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token  # Importa o modelo Token para obter tokens de autenticação
 
 from django.urls import reverse
 
@@ -12,20 +13,21 @@ from order.models import Order
 
 class TestOrderViewSet(APITestCase):
 
-    client = APIClient()
-
     def setUp(self):
+        self.client = APIClient()
         self.category = CategoryFactory(title='technology')
         self.product = ProductFactory(title='mouse', price=100, category=[self.category])
         self.order = OrderFactory(product=[self.product])
 
+        # Cria um usuário e obtém ou cria um token para ele
+        self.user = UserFactory()
+        self.token, _ = Token.objects.get_or_create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')  # Define o token nos cabeçalhos da solicitação
+
     def test_order(self):
-        response = self.client.get(
-            reverse('order-list', kwargs={'version': 'v1'})
-        )    
+        response = self.client.get(reverse('order-list', kwargs={'version': 'v1'}))    
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
 
         order_data = json.loads(response.content)
         self.assertEqual(order_data['results'][0]['product'][0]['title'], self.product.title)
@@ -34,11 +36,10 @@ class TestOrderViewSet(APITestCase):
         self.assertEqual(order_data['results'][0]['product'][0]['category'][0]['title'], self.category.title)        
 
     def test_create_order(self):
-        user = UserFactory()
         product = ProductFactory()
         data = json.dumps({
             'products_id': [product.id],
-            'user': user.id
+            'user': self.user.id
         })
 
         response = self.client.post(
@@ -47,8 +48,7 @@ class TestOrderViewSet(APITestCase):
             content_type='application/json'
         )
 
-        
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        created_order = Order.objects.get(user=user)
+        created_order = Order.objects.get(user=self.user)
 
--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,13 +1,17 @@
 
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
 class OrderViewSet(ModelViewSet):
-
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by('id')
+    authentication_classes = [TokenAuthentication]  # Adiciona autenticação por token
+    permission_classes = [IsAuthenticated]  # Requer autenticação para acessar as views
+
 
    
